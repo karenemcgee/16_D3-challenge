329fdf29b29f03c7bab6592aea271e4c5e9556e8
Karen McGee <karen.eliz.mcgee@gmail.com>
2019-09-28 17:47:50 -0500

    completed app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -13,14 +13,13 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
-var svg = d3
-  .select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+var svg = d3.select("#scatter")
+            .append("svg")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
 
 // Append an SVG group
-var chartGroup = svg.append("g");
+var chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.csv("assets/js/data.csv")
     .then(function(healthData) {
@@ -32,27 +31,21 @@ d3.csv("assets/js/data.csv")
 
         console.log(healthData);
 
-        var xLinearScale = d3.scaleLinear().range([0, width]);
-        var yLinearScale = d3.scaleLinear().range([height, 0]);
-
-        var bottomAxis = d3.axisBottom(xLinearScale);
-        var leftAxis = d3.axisLeft(yLinearScale);
-
         // Configure axes
         var xMin = d3.min(healthData, function(d) {
-            return d.healthcare;
+            return d.poverty;
         });
 
         var xMax = d3.max(healthData, function(d) {
-            return d.healthcare;
+            return d.poverty;
         });
 
         var yMin = d3.min(healthData, function(d) {
-            return d.poverty;
+            return d.healthcare;
         });
 
         var yMax = d3.max(healthData, function(d) {
-            return d.poverty;
+            return d.healthcare;
         });
 
         console.log(xMin);
@@ -60,41 +53,35 @@ d3.csv("assets/js/data.csv")
         console.log(yMin);
         console.log(yMax);
 
-        xLinearScale.domain([xMin, xMax]);
-        yLinearScale.domain([yMin, yMax]);
+        var labelArea = 30
+
+        var xLinearScale = d3.scaleLinear().domain([(xMin - .5), xMax + 2]).range([0, width]);
+        var yLinearScale = d3.scaleLinear().domain([(yMin - 1), yMax + 2]).range([height, 0]);
+
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
 
         // Append axes
         chartGroup.append("g")
                   .attr("transform", `translate(0, ${height})`)
-        .call(bottomAxis);
+                  .call(bottomAxis);
 
         chartGroup.append("g").call(leftAxis);
 
         // Circles
         chartGroup.selectAll("circle")
-            .data(healthData)
-            .enter()
-            .append("circle")
-            .attr("cx", function(data, index) {
-                return xLinearScale(data.poverty)
-            })
-            .attr("cy", function(data, index) {
-                return yLinearScale(data.healthcare)
-            })
-            .attr("r", "15")
-            .attr("fill", "lightblue");
-
-        // //
-        // var circlesGroup = chartGroup.selectAll("circle")
-        // .data(healthData)
-        // .enter()
-        // .append("circle")
-        // .attr("cx", d => xLinearScale(d.healthcare +1.5))
-        // .attr("cy", d => yLinearScale(d.poverty +0.3))
-        // .attr("r", "12")
-        // .attr("fill", "blue")
-        // .attr("opacity", .5)
-        // //
+                  .data(healthData)
+                  .enter()
+                  .append("circle")
+                  .attr("cx", function(data) {
+                      return xLinearScale(data.poverty)
+                  })
+                  .attr("cy", function(data) {
+                      return yLinearScale(data.healthcare)
+                  })
+                  .attr("r", "15")
+                  .attr("fill", "mediumturquoise")
+                  .attr("opacity", .7);
 
         // Abbreviations in Circles
         chartGroup.append("text")
@@ -118,20 +105,16 @@ d3.csv("assets/js/data.csv")
         chartGroup.append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 0-margin.left + 40)
-            .attr("x", 0 - height/2)
+            .attr("x", 0 - height/1.5)
             .attr("dy","1em")
             .attr("class", "axis-text")
             .text("Lacks Healthcare (%)");
 
         chartGroup.append("text")
-            .attr("transform", "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")")
+            .attr("transform", "translate(" + width / 2.25 + " ," + (height + margin.top + 30) + ")")
             .attr("class", "axis-text")
             .text("In Poverty (%)");
         })
+        
     .catch(function(error){
-     // handle error   
         })
-
-// d3.csv("assets/js/data.csv", function(error, data) {
-//     if (error) throw error;
-
